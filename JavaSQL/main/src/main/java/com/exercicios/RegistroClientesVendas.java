19248b3eee85550809ad3e01f64016b2daa73ae5
Adler <adlervitor982@gmail.com>
2023-12-05 14:46:09 -0300

    Retirando imports initilizados

--- JavaSQL/main/src/main/java/com/exercicios/RegistroClientesVendas.java
+++ JavaSQL/main/src/main/java/com/exercicios/RegistroClientesVendas.java
@@ -3,7 +3,6 @@ package com.exercicios;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class RegistroClientesVendas {
